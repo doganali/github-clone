71aa41ee3ff1c846415cda5f4f907a25a687aa74
doganali <ali.dogan@bayzat.com>
2023-11-01 17:10:31 +0100

    Move search calls into SearchResultsPage

--- src/app/dashboard/DashboardPage.tsx
+++ src/app/dashboard/DashboardPage.tsx
@@ -1,16 +1,11 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import SearchBar from '../../design-system/components/SearchBar';
 import Image from '../../design-system/components/Image';
 import githubLogo from '../../assets/github-mark.png';
-import {searchRepositories, searchUsers } from "../../api/service/GithubService";
-import {GithubRepositoriesPayload} from "../../api/model/response/GithubRepository";
-import {GithubUsersPayload} from "../../api/model/response/GithubUser";
-import { useNavigate } from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
 
 const DashboardPage: React.FC = () => {
     const [searchQuery, setSearchQuery] = useState('');
-    const [, setRepositorySearchResults] = useState<GithubRepositoriesPayload>();
-    const [, setUserSearchResults] = useState<GithubUsersPayload>();
     let navigate = useNavigate();
 
     const handleSearchChange = (newValue: string) => {
@@ -18,34 +13,22 @@ const DashboardPage: React.FC = () => {
         // Add debounce later?
     };
 
-    const handleSearch = async (query: string) => {
-        console.log('Searching for:', query);
-
-        // TODO: add pagination later
-        const [repoResults, userResults] = await Promise.all([
-            searchRepositories(query),
-            searchUsers(query)
-        ]);
-
-        console.log('Repository Search Results: ', repoResults);
-        console.log('User Search Results: ', userResults);
-        setRepositorySearchResults(repoResults);
-        setUserSearchResults(userResults);
-        navigate('/results', { state: { repoResults, userResults } });
+    const handleSearch = async () => {
+        navigate('/results', {state: {searchQuery}});
     };
 
     return (
-        <div style={{ textAlign: 'center', padding: '50px' }}>
+        <div style={{textAlign: 'center', padding: '50px'}}>
             <Image
                 src={githubLogo}
                 alt="GitHub Logo"
-                style={{ width: '250px', margin: '0 auto 30px' }}
+                style={{width: '250px', margin: '0 auto 30px'}}
             />
-            <div style={{ maxWidth: '600px', margin: '0 auto' }}>
+            <div style={{maxWidth: '600px', margin: '0 auto'}}>
                 <SearchBar
                     value={searchQuery}
                     onChange={handleSearchChange}
-                    onSearch={() => handleSearch(searchQuery)}
+                    onSearch={() => handleSearch()}
                     placeholder="Search for repositories or users..."
                 />
             </div>
--- src/app/search-results/SearchResultsPage.tsx
+++ src/app/search-results/SearchResultsPage.tsx
@@ -1,40 +1,50 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useLocation} from 'react-router-dom';
+import {searchRepositories, searchUsers} from "../../api/service/GithubService";
 import {GithubRepositoriesPayload} from "../../api/model/response/GithubRepository";
 import {GithubUsersPayload} from "../../api/model/response/GithubUser";
 
 const SearchResultsPage: React.FC = () => {
     const location = useLocation();
-    const state = location.state as {
-        repoResults: GithubRepositoriesPayload;
-        userResults: GithubUsersPayload;
-    };
+    const [repoResults, setRepoResults] = useState<GithubRepositoriesPayload>();
+    const [userResults, setUserResults] = useState<GithubUsersPayload>();
+    const [isLoading, setIsLoading] = useState(true);
 
-    const repoResults = state.repoResults
-    const userResults = state.userResults
+    const searchTerm = location.state.searchQuery;
+
+    useEffect(() => {
+        console.log('searching: ', searchTerm)
+        if (searchTerm) {
+            setIsLoading(true);
+            Promise.all([
+                searchRepositories(searchTerm),
+                searchUsers(searchTerm),
+            ]).then(([repoResponse, userResponse]) => {
+                setRepoResults(repoResponse);
+                setUserResults(userResponse);
+                setIsLoading(false);
+            });
+        }
+    }, [searchTerm]);
+
+    if (isLoading) {
+        // TODO: implement a loading UI
+        return <div>Loading results...</div>;
+    }
 
     return (
         <div>
             <h1>Repository Results</h1>
             <ul>
-                {repoResults.items.map((repo) => (
-                    <li key={repo.id}>
-                        <a href={repo.html_url} target="_blank" rel="noopener noreferrer">
-                            {repo.full_name}
-                        </a>
-                        - Stars: {repo.stargazers_count}
-                    </li>
+                {repoResults?.items.map(repo => (
+                    <li key={repo.id}>{repo.full_name}</li>
                 ))}
             </ul>
+
             <h1>User Results</h1>
             <ul>
-                {userResults.items.map((user) => (
-                    <li key={user.id}>
-                        <a href={user.html_url} target="_blank" rel="noopener noreferrer">
-                            {user.login}
-                        </a>
-                        - Followers: {user.followers_url.length}
-                    </li>
+                {userResults?.items.map(user => (
+                    <li key={user.id}>{user.login}</li>
                 ))}
             </ul>
         </div>
