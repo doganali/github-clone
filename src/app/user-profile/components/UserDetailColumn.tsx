3d813fc06f2198a470bd1e71069c582e80216297
doganali <ali.dogan@bayzat.com>
2023-11-08 14:01:20 +0100

    Style user detail column

--- src/app/user-profile/components/UserDetailColumn.tsx
+++ src/app/user-profile/components/UserDetailColumn.tsx
@@ -1,24 +1,82 @@
 import React from 'react';
-import { GitHubUser } from '../../../api/model/response/GithubUser';
+import {GitHubUser} from '../../../api/model/response/GithubUser';
 
 interface UserDetailColumnProps {
     user: GitHubUser;
 }
 
-const UserDetailColumn: React.FC<UserDetailColumnProps> = ({ user }) => {
+const UserDetailColumn: React.FC<UserDetailColumnProps> = ({user}) => {
+    const styles: { [key: string]: React.CSSProperties } = {
+        container: {
+            paddingTop: '24px',
+            paddingLeft: '16px',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'start'
+        },
+        avatar: {
+            width: '200px',
+            height: '200px',
+            borderRadius: '50%',
+            alignSelf: 'center'
+        },
+        followButton: {
+            width: '100%',
+            margin: '10px 0',
+            padding: '10px',
+            borderRadius: '20px',
+            boxShadow: '0px 2px 5px rgba(0, 0, 0, 0.2)',
+            border: 'none',
+            cursor: 'pointer',
+            backgroundColor: '#9b9a9a',
+            color: 'black'
+        },
+        statsRow: {
+            display: 'flex',
+            justifyContent: 'center',
+            width: '100%',
+            marginBottom: '10px'
+        },
+        stat: {
+            margin: '0 5px',
+        },
+        bullet: {
+            margin: '0 5px',
+            fontWeight: 'bold'
+        },
+        emailRow: {
+            marginTop: '10px'
+        }, userName: {
+            marginBottom: '0em'
+        },
+        userLogin: {
+            marginTop: '0em',
+            color: '#808080',
+        }
+    };
+
+
+    function mockFollowerCount() {
+        return 42
+    }
+
+    function mockFollowingCount() {
+        return 345;
+    }
+
     return (
-        <div style={{ flex: 1 }}>
-            <img src={user.avatar_url} alt={`${user.login} avatar`} style={{ width: '100px', height: '100px', borderRadius: '50%' }} />
-            <h1>{user.name || user.login}</h1>
-            <p>{`@${user.login}`}</p>
-            <p>{user.bio}</p>
-            <button style={{ margin: '10px 0' }}>Follow</button>
-            <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
-                <span>{user.followers_url.length} Followers</span>
-                <span>{user.following_url.length} Following</span>
-                <span>Stars</span>
+        <div style={styles.container}>
+            <img src={user.avatar_url} alt={`${user.login} avatar`} style={styles.avatar}/>
+            <h1 style={styles.userName}>{user.name || user.login}</h1>
+            <p style={styles.userLogin}>{`@${user.login}`}</p>
+            {user.bio && <p>{user.bio}</p>}
+            <button style={styles.followButton}>Follow</button>
+            <div style={styles.statsRow}>
+                <span style={styles.stat}>{mockFollowerCount()} followers</span>
+                <span style={styles.bullet}>&bull;</span>
+                <span style={styles.stat}>{mockFollowingCount()} following</span>
             </div>
-            <p>{user.email}</p>
+            {user.email && <p style={styles.emailRow}>{user.email}</p>}
         </div>
     );
 };
